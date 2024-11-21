544871ae8c9ec80d7b17442d250477d58b7bb6ad
Kosy <kosysteve03@gmail.com>
2024-11-21 11:55:24 +0000

    Bottom navigation

---
+++ matcron/lib/animated_notch_bottom_bar/animated_notch_bottom_bar.dart
@@ -0,0 +1,6 @@
+library matcron;
+
+export 'package:matcron/core/models/bottom_bar_item_model.dart';
+export 'package:matcron/core/notch_bottom_bar.dart';
+export 'package:matcron/core/notch_bottom_bar_controller.dart';
+export 'package:matcron/core/bottom_bar_painter.dart';
