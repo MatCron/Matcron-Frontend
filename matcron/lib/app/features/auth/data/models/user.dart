388b9b634141d481fefe5427301abd9be6709397
Jason Njoku <JasNjoku@gmail.com>
2024-11-24 22:29:05 +0000

    Modify user models

--- matcron/lib/app/features/auth/data/models/user.dart
+++ matcron/lib/app/features/auth/data/models/user.dart
@@ -2,23 +2,31 @@ import 'package:matcron/app/features/auth/domain/entities/user.dart';
 
 class UserModel extends UserEntity {
   const UserModel({
-    int ? id,
+    String ? id,
     String ? firstName,
     String ? lastName,
-    int ? organizationId,
+    String ? organizationId,
     String ? email,
     bool ? emailVerified,
     String ? image,
-  });
+  }) : super(
+        id: id ?? "",
+        firstName: firstName ?? "",
+        lastName: lastName ?? "",
+        organizationId: organizationId ?? "",
+        email: email ?? "",
+        emailVerified: emailVerified ?? false,
+        image: image ?? "",
+      );
 
   factory UserModel.fromJson(Map <String, dynamic> map) {
     return UserModel(
-      id: map["id"] ?? "",
+      id: map["userId"] ?? "",
       firstName: map["firstName"] ?? "",
       lastName: map["lastName"] ?? "",
       organizationId: map["organizationId"] ?? "",
       email: map["email"] ?? "",
-      emailVerified: map["emailVerified"] ?? "",
+      emailVerified: map["emailVerified"] ?? false,
       image: map["image"] ?? "",
     );
   } 
