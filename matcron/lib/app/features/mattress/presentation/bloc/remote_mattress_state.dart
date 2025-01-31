b99937b4bc9089447d70a02e863d32e830837fb3
Jason Njoku <JasNjoku@gmail.com>
2025-01-31 03:14:46 +0000

    see groups in mattress page

--- matcron/lib/app/features/mattress/presentation/bloc/remote_mattress_state.dart
+++ matcron/lib/app/features/mattress/presentation/bloc/remote_mattress_state.dart
@@ -1,16 +1,18 @@
 import 'package:dio/dio.dart';
 import 'package:equatable/equatable.dart';
+import 'package:matcron/app/features/group/domain/entities/group_entity.dart';
 import 'package:matcron/app/features/mattress/domain/entities/mattress.dart';
 import 'package:matcron/app/features/type/domain/entities/mattress_type.dart';
 
 abstract class RemoteMattressState extends Equatable {
   final List<MattressEntity>? mattresses;
   final List<MattressTypeEntity>? types;
+  final List<GroupEntity>? groups;
   final MattressEntity? mattress;
   final String? uid;
   final DioException? exception;
 
-  const RemoteMattressState({this.mattress, this.mattresses, this.types, this.uid, this.exception});
+  const RemoteMattressState({this.mattress, this.mattresses, this.types, this.uid, this.exception, this.groups});
 
   @override
   List<Object> get props {
@@ -33,7 +35,7 @@ class RemoteMattressInitial extends RemoteMattressState {
 }
 
 class RemoteMattressesDone extends RemoteMattressState {
-  const RemoteMattressesDone(List<MattressEntity> mattresses, List<MattressTypeEntity> types, String? uid) : super(mattresses: mattresses, types: types, uid: uid);
+  const RemoteMattressesDone(List<MattressEntity> mattresses, List<MattressTypeEntity> types, String? uid, List<GroupEntity> groups) : super(mattresses: mattresses, types: types, uid: uid, groups: groups);
 }
 
 class RemoteMattressesException extends RemoteMattressState {
