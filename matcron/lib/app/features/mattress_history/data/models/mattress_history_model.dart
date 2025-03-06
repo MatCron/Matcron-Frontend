6af2c3acfc3db817fa4cc74cc5204270f4e606f0
Jason Njoku <JasNjoku@gmail.com>
2025-03-06 15:31:07 +0000

    Modify model to include original data and new data

--- matcron/lib/app/features/mattress_history/data/models/mattress_history_model.dart
+++ matcron/lib/app/features/mattress_history/data/models/mattress_history_model.dart
@@ -8,6 +8,8 @@ class MattressHistoryModel extends MattressHistoryEntity {
       {super.id,
       super.mattressId,
       super.details,
+      super.oldDetails,
+      super.newDetails,
       super.status,
       super.timeStamp,
       super.type});
@@ -19,11 +21,21 @@ class MattressHistoryModel extends MattressHistoryEntity {
       details: map['details'] != null && map['details'] is String
           ? jsonDecode(map['details'])['Detail']
           : "",
+      oldDetails: _convertToString(map['details'], 'Original'),
+      newDetails: _convertToString(map['details'], 'New'),
       status: map['status'] as int,
       type: map['type'] as int,
       timeStamp: map['timeStamp'] != null
           ? DateFormat("MM/dd/yyyy HH:mm:ss").parse(map['timeStamp'])
           : null,
     );
+  }
+
+  static String _convertToString(dynamic details, String key) {
+    if (details != null && details is String) {
+      var decoded = jsonDecode(details);
+      return decoded[key] is int ? decoded[key].toString() : decoded[key] ?? "";
+    }
+    return "";
   }
 }
