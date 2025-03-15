cc0697d9c170c52e7826c06c5d6477ea4bcf1b4b
Jason Njoku <JasNjoku@gmail.com>
2025-03-15 12:53:02 +0000

    Update secure storage package

--- matcron/lib/core/resources/authorization.dart
+++ matcron/lib/core/resources/authorization.dart
@@ -9,7 +9,13 @@ class AuthorizationService {
   }
 
   Future<String?> getToken() async {
-    return await secureStorage.read(key: 'authToken');
+    try {
+      return await secureStorage.read(key: 'authToken');
+    } catch (e) {
+      print("Secure storage error: $e");
+      await secureStorage.deleteAll(); // Reset storage if decryption fails
+      return null;
+    }
   }
 
   void deleteToken() async {
@@ -28,23 +34,23 @@ class AuthorizationService {
   }
 
   Future<int?> getUserType() async {
-  String? token = await getToken();
-  if (token != null && token.isNotEmpty) {
-    var p = JwtDecoder.decode(token);
-    
-    // Safely parse the 'UserType' value
-    if (p.containsKey('UserType')) {
-      try {
-        return int.parse(p['UserType'].toString()); // Convert to string first, then parse
-      } catch (e) {
-        print("Error parsing UserType: $e");
-        return null;
+    String? token = await getToken();
+    if (token != null && token.isNotEmpty) {
+      var p = JwtDecoder.decode(token);
+      print(p);
+      // Safely parse the 'UserType' value
+      if (p.containsKey('UserType')) {
+        try {
+          return int.parse(
+              p['UserType'].toString()); // Convert to string first, then parse
+        } catch (e) {
+          print("Error parsing UserType: $e");
+          return null;
+        }
       }
     }
+    return null;
   }
-  return null;
-}
-
 
   /// Check if token is expired
   Future<bool> isTokenExpired() async {
