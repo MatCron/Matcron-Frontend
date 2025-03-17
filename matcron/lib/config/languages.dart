673d2c60853ed0f256179afe7d5e94817e06f46c
Jason Njoku <JasNjoku@gmail.com>
2025-03-17 17:57:09 +0000

    update files

---
+++ matcron/lib/config/languages.dart
@@ -0,0 +1,61 @@
+final languages = {
+  "EN": {
+    "Header": {
+      "Dashboard": "Dashboard",
+      "Mattress": "Mattress",
+      "Types": "Types",
+      "Group": "Group"
+    },
+
+    "Login": {
+      "WelcomeToMatcron!": "Welcome to Matcron!",
+      "EnterEmail": "Enter email",
+      "EnterPassword": "Enter password",
+      "ForgotPassword": "Forgot Password",
+      "LogIn": "Log In"
+    },
+
+    "Dashboard": {
+      "MattressLifecycle": "Mattress Lifecycle",
+      "MattressMaintenance": "Mattress Maintenance",
+      "RequesWash": "Request Wash",
+    },
+
+    "Mattress": {
+      "ImportGroup": "Import Group",
+      "PreviewName": "Name",
+      "PreviewDesc": "Description",
+      "PreviewCount": "Mattress Count",
+      "PreviewStatus": "Status",
+      "PreviewPurpose": "Transfer Purpose",
+      "PreviewCancel": "PreviewCancel",
+      "PreviewImport": "PreviewImport",
+      "ImportSuccess": "Mattresses imported successfully!",
+      "MattressAddedToGroup": "Mattresses added to group successfully!",
+      "MattressAlreadyAdded": "Mattresses already added to group.",
+      "FilterByStatus": "Filter by Status",
+      "SearchMattress": "Search Mattress",
+      "TypeHeader": "Type",
+      "LocationHeader": "Location",
+      "StatusHeader": "Status",
+      "NoMattress": "No mattresses available",
+      "Rotate": "Rotate",
+      "Days": "days",
+      "EndOfLifecycle": "End of Lifecycle",
+      "Organization": "Organization",
+      "MattressEdit": "Edit",
+      "MattressMore": "More",
+
+      "AddMattressTitle": "Mattress",
+      "EnterLocation": "Enter Location",
+      "SelectMattressType": "Search Mattress Type",
+      "AddCustomType": "+ Add custom type",
+      "SelectDate": "Select Date",
+      "Generate RFID": "Generate RFID",
+      
+    }
+
+  },
+};
+
+//handle constants
