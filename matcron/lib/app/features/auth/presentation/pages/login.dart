87c4dc63af3389750f798435f3a79f3fc13e97ae
Jason Njoku <JasNjoku@gmail.com>
2025-03-18 13:59:51 +0000

    change 3

--- matcron/lib/app/features/auth/presentation/pages/login.dart
+++ matcron/lib/app/features/auth/presentation/pages/login.dart
@@ -9,7 +9,7 @@ import 'package:matcron/app/features/auth/presentation/bloc/auth/remote/login/re
 import 'package:matcron/app/features/auth/presentation/bloc/auth/remote/remote_auth_state.dart';
 //import 'package:matcron/app/features/auth/presentation/pages/register.dart';
 //import 'package:matcron/app/injection_container.dart';
-import 'package:matcron/app/features/auth/presentation/pages/register.dart';
+//import 'package:matcron/app/features/auth/presentation/pages/register.dart';
 import 'package:matcron/app/injection_container.dart';
 import 'package:matcron/main.dart';
 import 'package:matcron/core/constants/constants.dart';
