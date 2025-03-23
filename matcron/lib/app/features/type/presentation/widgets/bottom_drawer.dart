abad2fe52d4b7e2f34c004cae62f75eed0787ea5
Jason Njoku <JasNjoku@gmail.com>
2025-03-23 18:50:40 +0000

    Change color of text for history

--- matcron/lib/app/features/type/presentation/widgets/bottom_drawer.dart
+++ matcron/lib/app/features/type/presentation/widgets/bottom_drawer.dart
@@ -292,7 +292,7 @@ class MattressTypeBottomDrawerState extends State<MattressTypeBottomDrawer> {
                                         Text("Location changed from '${item.oldDetails}' to '${item.newDetails}'",
                                           style: TextStyle(
                                               fontSize: 14,
-                                              color:theme.cardColor,
+                                              
                                               fontStyle: FontStyle.italic
                                               ),
                                         ),
@@ -301,7 +301,7 @@ class MattressTypeBottomDrawerState extends State<MattressTypeBottomDrawer> {
                                         Text("Status changed from '${mattressStatus[int.parse(item.oldDetails!)]['Text']}' to '${mattressStatus[int.parse(item.newDetails!)]['Text']}'",
                                           style: TextStyle(
                                               fontSize: 14,
-                                              color: theme.cardColor,
+                                              
                                               fontStyle: FontStyle.italic
                                               ),
                                         ),
@@ -313,7 +313,7 @@ class MattressTypeBottomDrawerState extends State<MattressTypeBottomDrawer> {
                                               : "No date",
                                           style: TextStyle(
                                               fontSize: 14,
-                                              color:theme.cardColor),
+                                              ),
                                         ),
                                       ],
                                     ),
